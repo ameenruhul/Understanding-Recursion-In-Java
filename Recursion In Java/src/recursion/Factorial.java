e1998e29257f91ef256eea387bf72ac366faf052
Ameen Ruhul Mohammad <ameensunny242@gmail.com>
2017-11-19 21:35:08 +0600

    test push

---
+++ Recursion In Java/src/recursion/Factorial.java
@@ -0,0 +1,16 @@
+package recursion;
+
+public class Factorial
+{
+
+    int fact (int n)
+    {
+         int result;
+         if(n ==1 )
+            return  1;
+         result = fact(n - 1) * n;
+         return result;
+
+    }
+}
+
